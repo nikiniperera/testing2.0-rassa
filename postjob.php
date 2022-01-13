abbcbcbe85ae52a7ca3bdfe8c2d7b14f5a3f9fdd
Yomindu Rajasuriya <51852251+Yomindu@users.noreply.github.com>
2022-01-13 11:40:37 +0530

    test

--- postjob.php
+++ postjob.php
@@ -117,29 +117,17 @@
 	  </div>
 	    <!--/.navbar-collapse-->
 	</nav>
-<div class="banner_1">
-	<div class="container">
-		<div id="search_wrapper1">
-		   <div id="search_form" class="clearfix">
-		    <h1>Start your job search</h1>
-		    <p>
-			 <input type="text" class="text" placeholder=" " value="Enter Keyword(s)" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Enter Keyword(s)';}">
-			 <input type="text" class="text" placeholder=" " value="Location" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Location';}">
-			 <label class="btn2 btn-2 btn2-1b"><input type="submit" value="Find Jobs"></label>
-			</p>
-           </div>
-		</div>
-   </div> 
-</div>	
+
 <div class="container">
     <div class="single">  
 	   <div class="form-container">
-        <h2>Upload Your Resume</h2>
+        <h2>Upload Your Job Details</h2>
+		<p1> confirmation of your post will be provide trough email. 
         <div class="search_form1">
 		    <form action="">
 			  <input type="text" class="text" placeholder=" " value="Email Address" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Email Address';}"><br>
 			  <input type="file" name="myfile">
-			  <input type="submit" value="Post your CV">
+			  <input type="submit" value="Post your Job">
 	        </form>
 	    </div>
     </div>
