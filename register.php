3fc00029bcd9786e5784f1c3703a29f3627c3c01
Yomindu Rajasuriya <51852251+Yomindu@users.noreply.github.com>
2022-01-13 10:00:28 +0530

    test

--- register.php
+++ register.php
@@ -117,20 +117,7 @@
 	  </div>
 	    <!--/.navbar-collapse-->
 	</nav>
-<div class="banner_1">
-	<div class="container">
-		<div id="search_wrapper1">
-		   <div id="search_form" class="clearfix">
-		    <h1>Start your job search</h1>
-		    <p>
-			 <input type="text" class="text" placeholder=" " value="Enter Keyword(s)" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Enter Keyword(s)';}">
-			 <input type="text" class="text" placeholder=" " value="Location" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Location';}">
-			 <label class="btn2 btn-2 btn2-1b"><input type="submit" value="Find Jobs"></label>
-			</p>
-           </div>
-		</div>
-   </div> 
-</div>	
+	
 <div class="container">
     <div class="single">  
 	   <div class="form-container">
