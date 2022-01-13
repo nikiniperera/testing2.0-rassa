195174eb5985a36e6df3d64f003c45e016dd5493
Yomindu Rajasuriya <51852251+Yomindu@users.noreply.github.com>
2022-01-13 16:35:10 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -100,8 +100,7 @@
     </div>
 </div>
 <div class="map">
-	<div style="width: 100%"><iframe width="720" height="240" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=720&amp;height=240&amp;hl=en&amp;q=kottawa+()&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/sport-gps/">bike gps</a></iframe></div>
-</div>
+	<div style="width: 100%"><iframe width="100%" height="240" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=240&amp;hl=en&amp;q=kottawa+()&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/sport-gps/">bike gps</a></iframe></div></div>
 <div class="footer">
 	<div class="container">
 		<div class="col-md-3 grid_3">
