55f4c4314a75fe7bc9f55ccc5ca53efed4a1067f
Yomindu Rajasuriya <51852251+Yomindu@users.noreply.github.com>
2022-01-13 08:16:48 +0530

    test

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <html>
 <head>
 <title>Rassa.lk</title>
+<link rel="icon" type="image/x-icon" href="images/logo2.png">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="keywords" content="Job Portal Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
