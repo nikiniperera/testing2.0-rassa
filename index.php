ba90914820fdc5e7edea0ea2e38b9a3d287e308d
Yomindu Rajasuriya <51852251+Yomindu@users.noreply.github.com>
2022-01-14 14:09:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -267,6 +267,15 @@
 	   </div>
 	   <div class="clearfix"> </div>
 	 </div>
+	 <ul class="pagination jobs_pagination">
+		<li class="disabled"><a href="#" aria-label="Previous"><span aria-hidden="true">«</span></a></li>
+		<li class="active"><a href="#">1 <span class="sr-only">(current)</span></a></li>
+		<li><a href="#">2</a></li>
+		<li><a href="#">3</a></li>
+		<li><a href="#">4</a></li>
+		<li><a href="#">5</a></li>
+		<li><a href="#" aria-label="Next"><span aria-hidden="true">»</span></a></li>
+	</ul>
 	 <div class="single">  
 	   
 	 </div>
