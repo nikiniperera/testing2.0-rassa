5974705875b9506f2222ec56dfa4ee2e41ccdbb8
Yomindu Rajasuriya <51852251+Yomindu@users.noreply.github.com>
2022-01-14 17:57:59 +0530

    Update postjob.php

--- postjob.php
+++ postjob.php
@@ -46,7 +46,7 @@
 		            </ul>
 		        </li>
 		        <li><a href="login.php">Login</a></li>
-		        <li><a href="postjob.php">Post Job</a></li>
+		        <li><a href="postjob.php">Post a Job</a></li>
 	        </ul>
 	    </div>
 	    <div class="clearfix"> </div>
