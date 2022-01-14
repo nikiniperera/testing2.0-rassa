bb0dbcee33ebc3ccf3f24c8492d3e58c00478364
Yomindu Rajasuriya <51852251+Yomindu@users.noreply.github.com>
2022-01-14 14:56:28 +0530

    Update index.php

--- index.php
+++ index.php
@@ -267,17 +267,18 @@
 	   </div>
 	   <div class="clearfix"> </div>
 	 </div>
-	 <ul class="pagination jobs_pagination">
-		<li class="disabled"><a href="#" aria-label="Previous"><span aria-hidden="true">«</span></a></li>
-		<li class="active"><a href="#">1 <span class="sr-only">(current)</span></a></li>
-		<li><a href="#">2</a></li>
-		<li><a href="#">3</a></li>
-		<li><a href="#">4</a></li>
-		<li><a href="#">5</a></li>
-		<li><a href="#" aria-label="Next"><span aria-hidden="true">»</span></a></li>
-	</ul>
+	 <div class="col-sm-8 row_1">
+		 <ul class="pagination jobs_pagination">
+			<li class="disabled"><a href="#" aria-label="Previous"><span aria-hidden="true">«</span></a></li>
+			<li class="active"><a href="#">1 <span class="sr-only">(current)</span></a></li>
+			<li><a href="#">2</a></li>
+			<li><a href="#">3</a></li>
+			<li><a href="#">4</a></li>
+			<li><a href="#">5</a></li>
+			<li><a href="#" aria-label="Next"><span aria-hidden="true">»</span></a></li>
+		</ul>
 	 <div class="single">  
-	   
+	 </div>  
 	 </div>
 </div>
 <div class="footer">
