31943dd1ff929f8251a6a4dceccaa38a02922428
Yomindu Rajasuriya <51852251+Yomindu@users.noreply.github.com>
2022-01-14 14:30:01 +0530

    Update index.php

--- index.php
+++ index.php
@@ -261,9 +261,7 @@
 						<span class="share1 google">Share</span>
 					</a>
 			   </div>
-			   <div class="clearfix"> </div>
-			   <div class="clearfix"> </div>
-			   <div class="clearfix"> </div>
+			   <div class="col_2">
 			   <ul class="pagination jobs_pagination">
 					<li class="disabled"><a href="#" aria-label="Previous"><span aria-hidden="true">«</span></a></li>
 					<li class="active"><a href="#">1 <span class="sr-only">(current)</span></a></li>
@@ -273,6 +271,7 @@
 					<li><a href="#">5</a></li>
 					<li><a href="#" aria-label="Next"><span aria-hidden="true">»</span></a></li>
 				</ul>
+			  </div>
 			  </div>
 			<div class="clearfix"> </div>
 		   </div>
